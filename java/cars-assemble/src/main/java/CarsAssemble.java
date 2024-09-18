c6b95ba3b4a38bba6a0a231a0f8facdf0b64b7d7
2khanh <khanhln.workspace@gmail.com>
2024-09-18 17:18:56 +0700

    [JAVA04][NEW] Car Assemble: solution

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,25 @@
 public class CarsAssemble {
 
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.productionRatePerHour() method");
+        int carProducedEachHour = 221;
+
+        if (speed < 0 || speed > 10)
+            return 0;
+        // Check for 100% success rate.
+        else if (speed >= 1 && speed <= 4)
+            return speed * carProducedEachHour;
+        // Check for 90% success rate.
+        else if (speed >= 5 && speed <= 8)
+            return speed * carProducedEachHour * 0.9;
+        // Check for 80% success rate.
+        else if (speed == 9)
+            return speed * carProducedEachHour * 0.8;
+        // Check for 77% success rate.
+        return speed * carProducedEachHour * 0.77;
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+        return (int)productionRatePerHour(speed) / 60;
     }
+
 }
