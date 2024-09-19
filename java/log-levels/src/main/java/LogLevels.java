0ab68d1013143f9eed9188b94cb59f9907e374fc
2khanh <khanhln.workspace@gmail.com>
2024-09-19 16:44:55 +0700

    [JAVA06][NEW] Log Levels: solution

--- java/log-levels/src/main/java/LogLevels.java
+++ java/log-levels/src/main/java/LogLevels.java
@@ -1,14 +1,38 @@
 public class LogLevels {
     
     public static String message(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLevels.message() method");
+        int colonIndex = logLine.indexOf(":");
+
+        if (colonIndex == -1) {
+            return "No message";
+        }
+        return logLine.substring(colonIndex + 1).trim();
     }
 
     public static String logLevel(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLevels.logLevel() method");
+        if (logLine.contains("INFO"))
+            return "info";
+        if (logLine.contains("WARNING"))
+            return "warning";
+        if (logLine.contains("ERROR"))
+            return "error";
+
+        return "Invalid level!";
     }
 
     public static String reformat(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLevels.reformat() method");
+        String infoSuffix = " (info)";
+        String warningSuffix = " (warning)";
+        String errorSuffix = " (error)";
+
+        if (logLine.contains("INFO"))
+            return message(logLine).concat(infoSuffix);
+        if (logLine.contains("WARNING"))
+            return message(logLine).concat(warningSuffix);
+        if (logLine.contains("ERROR"))
+            return message(logLine).concat(errorSuffix);
+
+        return "Invalid format!";
     }
+
 }
